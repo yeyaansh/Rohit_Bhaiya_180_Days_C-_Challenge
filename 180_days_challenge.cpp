4e239d66a334cc549259bac8af2e0c247ddffbee
Yeyaansh Patel <yeyaansh@gmail.com>
2023-09-21 18:53:38 +0530

    DAY-4

--- 180_days_challenge.cpp
+++ 180_days_challenge.cpp
@@ -1,12 +1,241 @@
 // #include <iostream>
+// using namespace std;
 // int main()
 // {
-//     std::cout << "Hello Coder Army";
+//     int package;
+//     cout << "Check your Package is Accepted or Rejected: ";
+//     cin >> package;
+//     if (package > 10)
+//         cout << "Accepted";
+//     else
+//         cout << "Rejected";
 // }
 
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout << "Hello Coder Army";
-}
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int marks;
+//     cout << "Enter you Marks to find out your result: ";
+//     cin >> marks;
+//     if (marks > 33)
+//     {
+//         cout << "Pass";
+//     }
+//     else
+//     {
+//         cout << "Fail";
+//     }
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a, b;
+//     cout << "Enter the value of 'a' and 'b' to find the greater one: ";
+//     cin >> a >> b;
+//     if (a > b)
+//         cout << "a is greater";
+//     else if (a < b)
+//         cout << "b is greater";
+//     else
+//         cout << "a is equal to b";
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int num;
+//     cout << "Enter the Number to find Even or Odd: ";
+//     cin >> num;
+//     if (num % 2 == 0)
+//         cout << "Even Number";
+//     else
+//         cout << "Odd Number";
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int num;
+//     cout << "Enter the number to find Positive, Negative or Zero: ";
+//     cin >> num;
+//     if (num > 0)
+//         cout << "Positive";
+//     else if (num < 0)
+//         cout << "Negative";
+//     else
+//         cout << "Zero";
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     char c;
+//     cout << "Enter the Character to Find Vowel or Consonant: ";
+//     cin >> c;
+//     if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+//         cout << "Vowel";
+//     else
+//         cout << "Consonant";
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int date;
+//     cout << "Enter to find out the Day (from 1 to 7): ";
+//     cin >> date;
+//     if (date == 1)
+//         cout << "Monday";
+//     else if (date == 2)
+//         cout << "Tuesday";
+//     else if (date == 3)
+//         cout << "Wednesday";
+//     else if (date == 4)
+//         cout << "Thursday";
+//     else if (date == 5)
+//         cout << "Friday";
+//     else if (date == 6)
+//         cout << "Saturday";
+//     else if (date == 7)
+//         cout << "Sunday";
+//     else
+//         cout << "Enter a valid Day!";
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int num;
+//     cout << "Enter the Number to print the square till the number: ";
+//     cin >> num;
+//     for (int count = 1; count <= num; count++)
+//         cout << count << " Square is: " << count * count << endl;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int num;
+//     cout << "Enter the number to print all even numbers till the number: ";
+//     cin >> num;
+//     for (int i = 2; i <= num; i++)
+//     {
+//         if (i % 2 == 0)
+//             cout << i << endl;
+//     }
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int num;
+//     cout << "Enter the number to print all even number (another method) till the number: ";
+//     cin >> num;
+//     for (int i = 2; i <= num; i = i + 2)
+//         cout << i << endl;
+// };
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<HOMEWORK>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int age;
+//     cout << "Enter your age to verify you are Adult or Teenager: ";
+//     cin >> age;
+//     if (age > 18)
+//         cout << "Adult";
+//     else
+//         cout << "Teenager";
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int month;
+//     cout << "Enter the number to find out the month: ";
+//     cin >> month;
+//     if (month == 1)
+//         cout << "January";
+//     else if (month == 2)
+//         cout << "February";
+//     else if (month == 3)
+//         cout << "March";
+//     else if (month == 4)
+//         cout << "April";
+//     else if (month == 5)
+//         cout << "May";
+//     else if (month == 6)
+//         cout << "June";
+//     else if (month == 7)
+//         cout << "July";
+//     else if (month == 8)
+//         cout << "August";
+//     else if (month == 9)
+//         cout << "September";
+//     else if (month == 10)
+//         cout << "October";
+//     else if (month == 11)
+//         cout << "November";
+//     else if (month == 12)
+//         cout << "December";
+//     else
+//         cout << "Invalid Month! Please enter between 1 to 12";
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int age;
+//     cout << "Enter your age to check for Discounts: ";
+//     cin >> age;
+//     if (age < 12 || age > 65)
+//         cout << "YES, Eligible for Discounts";
+//     else
+//         cout << "Not Eligible for Discounts";
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     for (int i = 1; i <= 20; i++)
+//         cout << "India will win the World Cup 2023" << endl;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int num;
+//     cout << "Enter the number to print all Odd Numbers till the Number: ";
+//     cin >> num;
+//     for (int count = 1; count < num; count++)
+//         if (count % 2 == 1)
+//             cout << count << endl;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int num;
+//     cout << "Enter the number to find out all the numbers which are divisible by 4 till the number: ";
+//     cin >> num;
+//     for (int count = 1; count <= num; count++)
+//         if (count % 4 == 0)
+//             cout << count << endl;
+// };
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Today is Day 3 and today I learned about Variable and DataTypes and Written my First Code in C++...
+Today is Day 4 and today I learned about if-else statement and loop in C++...
+Today's HW is:
+1.) If-else
+a. Two numbers are given, print the bigger number, It is given that both numbers can’t be the same.
+b. Age of a person is given, print Adult if his/her age is greater than 18, otherwise print Teenager.
+c. Take a number in input (ex n) and print the corresponding month to it. Ex: for n=1, print january, n=2, print feburary like this you need to give output. It is given that n will be greater than 0 and less than 13.
+d. Ticket Price Calculator: Create a program that asks the user for their age and checks if they qualify for a discounted ticket price (e.g., under 12 and over 65 get discounts), If they are eligible print “YES” else “NO”.
+
+2.) For Loop
+a. Print “India will win the World Cup 2023”, 20 times.
+b. Print all Odd numbers from 1 to n, take n as an input from the user.
+c. Print all numbers from 1 to n, which is divisible by 4. Take n as an input from the user.
