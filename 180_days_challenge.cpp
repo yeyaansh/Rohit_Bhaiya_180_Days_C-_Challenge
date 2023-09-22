7f662369b3540d222c0710d49191aa793fd863af
Yeyaansh Patel <yeyaansh@gmail.com>
2023-09-22 22:27:46 +0530

    DAY-5

--- 180_days_challenge.cpp
+++ 180_days_challenge.cpp
@@ -2,218 +2,48 @@
 // using namespace std;
 // int main()
 // {
-//     int package;
-//     cout << "Check your Package is Accepted or Rejected: ";
-//     cin >> package;
-//     if (package > 10)
-//         cout << "Accepted";
-//     else
-//         cout << "Rejected";
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int marks;
-//     cout << "Enter you Marks to find out your result: ";
-//     cin >> marks;
-//     if (marks > 33)
-//     {
-//         cout << "Pass";
-//     }
-//     else
-//     {
-//         cout << "Fail";
-//     }
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int a, b;
-//     cout << "Enter the value of 'a' and 'b' to find the greater one: ";
-//     cin >> a >> b;
-//     if (a > b)
-//         cout << "a is greater";
-//     else if (a < b)
-//         cout << "b is greater";
-//     else
-//         cout << "a is equal to b";
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int num;
-//     cout << "Enter the Number to find Even or Odd: ";
-//     cin >> num;
-//     if (num % 2 == 0)
-//         cout << "Even Number";
-//     else
-//         cout << "Odd Number";
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int num;
-//     cout << "Enter the number to find Positive, Negative or Zero: ";
-//     cin >> num;
-//     if (num > 0)
-//         cout << "Positive";
-//     else if (num < 0)
-//         cout << "Negative";
-//     else
-//         cout << "Zero";
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     char c;
-//     cout << "Enter the Character to Find Vowel or Consonant: ";
-//     cin >> c;
-//     if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
-//         cout << "Vowel";
-//     else
-//         cout << "Consonant";
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int date;
-//     cout << "Enter to find out the Day (from 1 to 7): ";
-//     cin >> date;
-//     if (date == 1)
-//         cout << "Monday";
-//     else if (date == 2)
-//         cout << "Tuesday";
-//     else if (date == 3)
-//         cout << "Wednesday";
-//     else if (date == 4)
-//         cout << "Thursday";
-//     else if (date == 5)
-//         cout << "Friday";
-//     else if (date == 6)
-//         cout << "Saturday";
-//     else if (date == 7)
-//         cout << "Sunday";
-//     else
-//         cout << "Enter a valid Day!";
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int num;
-//     cout << "Enter the Number to print the square till the number: ";
-//     cin >> num;
-//     for (int count = 1; count <= num; count++)
-//         cout << count << " Square is: " << count * count << endl;
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int num;
-//     cout << "Enter the number to print all even numbers till the number: ";
-//     cin >> num;
-//     for (int i = 2; i <= num; i++)
-//     {
-//         if (i % 2 == 0)
-//             cout << i << endl;
-//     }
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int num;
-//     cout << "Enter the number to print all even number (another method) till the number: ";
-//     cin >> num;
-//     for (int i = 2; i <= num; i = i + 2)
-//         cout << i << endl;
+//     for (int count = 101; count <= 200; count++)
+//         cout << count << endl;
 // };
 
-// <<<<<<<<<<<<<<<<<<<<<<<<<<HOMEWORK>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
 // #include <iostream>
 // using namespace std;
 // int main()
 // {
-//     int age;
-//     cout << "Enter your age to verify you are Adult or Teenager: ";
-//     cin >> age;
-//     if (age > 18)
-//         cout << "Adult";
-//     else
-//         cout << "Teenager";
+//     char name;
+//     cout << "Printing Characters from 'a' to 'z': " << endl;
+//     for (name = 'a'; name <= 'z'; name++)
+//         cout << name << endl;
 // }
 
 // #include <iostream>
 // using namespace std;
 // int main()
 // {
-//     int month;
-//     cout << "Enter the number to find out the month: ";
-//     cin >> month;
-//     if (month == 1)
-//         cout << "January";
-//     else if (month == 2)
-//         cout << "February";
-//     else if (month == 3)
-//         cout << "March";
-//     else if (month == 4)
-//         cout << "April";
-//     else if (month == 5)
-//         cout << "May";
-//     else if (month == 6)
-//         cout << "June";
-//     else if (month == 7)
-//         cout << "July";
-//     else if (month == 8)
-//         cout << "August";
-//     else if (month == 9)
-//         cout << "September";
-//     else if (month == 10)
-//         cout << "October";
-//     else if (month == 11)
-//         cout << "November";
-//     else if (month == 12)
-//         cout << "December";
-//     else
-//         cout << "Invalid Month! Please enter between 1 to 12";
+//     int n = 10;
+//     cout << "Printing Number from 10 to 1: \n";
+//     for (n; n >= 1; n--)
+//         cout << n << endl;
 // }
 
 // #include <iostream>
 // using namespace std;
 // int main()
 // {
-//     int age;
-//     cout << "Enter your age to check for Discounts: ";
-//     cin >> age;
-//     if (age < 12 || age > 65)
-//         cout << "YES, Eligible for Discounts";
-//     else
-//         cout << "Not Eligible for Discounts";
+//     cout << "Printing number from 1 to 100 with gaps of 3 in them: \n";
+//     for (int num = 1; num <= 100; num = num + 3)
+//         cout << num << endl;
 // }
 
 // #include <iostream>
 // using namespace std;
 // int main()
 // {
-//     for (int i = 1; i <= 20; i++)
-//         cout << "India will win the World Cup 2023" << endl;
+//     int table;
+//     cout << "Enter the number to print its table: ";
+//     cin >> table;
+//     for (int count = 1; count <= 10; count++)
+//         cout << table << " * " << count << " = " << table * count << endl;
 // }
 
 // #include <iostream>
@@ -221,11 +51,36 @@
 // int main()
 // {
 //     int num;
-//     cout << "Enter the number to print all Odd Numbers till the Number: ";
+//     cout << "Enter the number to print its table(in other way): ";
 //     cin >> num;
-//     for (int count = 1; count < num; count++)
-//         if (count % 2 == 1)
-//             cout << count << endl;
+//     for (int i = num; i <= num * 10; i = i + num)
+//         cout << i << endl;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int num, power, answer = 1;
+//     cout << "Enter the number: ";
+//     cin >> num;
+//     cout << "Enter it's power: ";
+//     cin >> power;
+//     for (int i = 1; i <= power; i++)
+//         answer = answer * num;
+//     cout << num << " to the power " << power << " is: " << answer;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int num, sum = 0;
+//     cout << "Enter the number to print its sum: ";
+//     cin >> num;
+//     for (int i = 1; i <= num; i++)
+//         sum = sum + i;
+//     cout << "Sum till " << num << " is: " << sum;
 // }
 
 // #include <iostream>
@@ -233,9 +88,130 @@
 // int main()
 // {
 //     int num;
-//     cout << "Enter the number to find out all the numbers which are divisible by 4 till the number: ";
+//     long long fact = 1;
+//     cout << "Enter the number to find its factorial: ";
 //     cin >> num;
-//     for (int count = 1; count <= num; count++)
-//         if (count % 4 == 0)
-//             cout << count << endl;
+//     for (int i = 1; i <= num; i++)
+//         fact = fact * i;
+//     cout << "Factorial of " << num << " is: " << fact << endl;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int p_check;
+//     cout << "Enter the number to check Prime or Not: ";
+//     cin >> p_check;
+//     if (p_check < 2)
+//     {
+//         cout << "Not a Prime Number";
+//         return 0;
+//     }
+//     else
+//     {
+//         for (int count = 2; count < p_check; count++)
+//         {
+//             if (p_check % count == 0)
+//             {
+//                 cout << "Not a Prime Number";
+//                 return 0;
+//             }
+//         }
+//         cout << "It's a Prime Number";
+//     }
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int num, fib, first = 0, second = 1;
+//     cout << "Enter the terms to print Fibonacci Series: ";
+//     cin >> num;
+//     if (num == 1)
+//         cout << "0";
+//     else if (num == 2)
+//         cout << "0" << endl
+//              << "1";
+//     else
+//     {
+//         cout << "0" << endl
+//              << "1\n";
+//         for (int i = 1; i < num - 1; i++)
+//         {
+//             fib = first + second;
+//             first = second;
+//             second = fib;
+//             cout << fib << endl;
+//         }
+//     }
+// };
+
+//<<<<<<<<<<<<<<<<<<<<<<HOMEWORK>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int num;
+//     cout << "Printing Number from 250 to 280 with the help of 'for' loop: \n";
+//     for (num = 250; num <= 280; num++)
+//         cout << num << endl;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     char c = 'A';
+//     cout << "Printing Character from A to Z with the help of 'for' loop: \n";
+//     for (c; c <= 'Z'; c++)
+//         cout << c << endl;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     char c = 'Z';
+//     cout << "Printing character from Z to A with the help of 'for' loop: \n";
+//     for (c; c >= 'A'; c--)
+//         cout << c << endl;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int num = 220;
+//     cout << "There is an Arithmetic Progression, First number is 220 and Common Difference is 7. So printing all the numbers from 220 to 730: \n";
+//     for (num; num <= 720; num = num + 7)
+//         cout << num << endl;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int num;
+//     long long sum = 0;
+//     cout << "Enter the number to Print it's Sum of Squares till the number: ";
+//     cin >> num;
+//     for (int i = 1; i <= num; i++)
+//         sum = sum + (i * i);
+//     cout << "Your sum is: " << sum;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int num;
+//     long long sum = 0;
+//     cout << "Enter the number to Print it's Sum of Cube till the number: ";
+//     cin >> num;
+//     for (int i = 1; i <= num; i++)
+//         sum = sum + (i * i * i);
+//     cout << "Your sum is: " << sum;
 // };
--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-Today is Day 4 and today I learned about if-else statement and loop in C++...
+Today is Day 5 and today I learned about
 Today's HW is:
-1.) If-else
-a. Two numbers are given, print the bigger number, It is given that both numbers can’t be the same.
-b. Age of a person is given, print Adult if his/her age is greater than 18, otherwise print Teenager.
-c. Take a number in input (ex n) and print the corresponding month to it. Ex: for n=1, print january, n=2, print feburary like this you need to give output. It is given that n will be greater than 0 and less than 13.
-d. Ticket Price Calculator: Create a program that asks the user for their age and checks if they qualify for a discounted ticket price (e.g., under 12 and over 65 get discounts), If they are eligible print “YES” else “NO”.
 
-2.) For Loop
-a. Print “India will win the World Cup 2023”, 20 times.
-b. Print all Odd numbers from 1 to n, take n as an input from the user.
-c. Print all numbers from 1 to n, which is divisible by 4. Take n as an input from the user.
+1. Print number from 280 to 250 with the help of for loop.
+2. Print char from ‘A’ to ‘Z’ with the help of a for loop.
+3. Print char from ‘Z’ to ‘A’ with the help of a for loop.
+4. There is an Arithmetic Progression, First number is 220 and Common Difference is 7. So print all the numbers from 220 to 730 which follow the AP.
+5. Print Sum of square of first n natural number.
+6. Print Sum of cube of first n natural number
+7. Print n’th Fibonacci number.
