3b095e622e5283d0980466fcece5bbe8e042c921
Yeyaansh Patel <yeyaansh@gmail.com>
2023-09-25 15:51:08 +0530

    DAY-8

--- 180_days_challenge.cpp
+++ 180_days_challenge.cpp
@@ -1,217 +1,259 @@
+//  * * * * *
+//  * * * * *
+//  * * * * *
+//  * * * * *
+//  * * * * *
 // #include <iostream>
 // using namespace std;
 // int main()
 // {
-//     for (int count = 101; count <= 200; count++)
-//         cout << count << endl;
+//     int row, col;
+//     for (row = 1; row <= 5; row++)
+//     {
+//         for (col = 1; col <= 5; col++)
+//         {
+//             cout << " * ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+//  10 10 10 10 10
+//  10 10 10 10 10
+//  10 10 10 10 10
+//  10 10 10 10 10
+//  10 10 10 10 10
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     for (int row = 1; row <= 4; row++)
+//     {
+//         for (int col = 1; col <= 5; col++)
+//         {
+//             cout << "10 ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+//  1 1 1 1 1
+//  2 2 2 2 2
+//  3 3 3 3 3
+//  4 4 4 4 4
+//  5 5 5 5 5
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     for (int row = 1; row <= 5; row++)
+//     {
+//         for (int col = 1; col <= 5; col++)
+//         {
+//             cout << row << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+//  1 2 3 4 5
+//  1 2 3 4 5
+//  1 2 3 4 5
+//  1 2 3 4 5
+//  1 2 3 4 5
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     for (int row = 1; row <= 5; row++)
+//     {
+//         for (int col = 1; col <= 5; col++)
+//         {
+//             cout << col << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+//  5 4 3 2 1
+//  5 4 3 2 1
+//  5 4 3 2 1
+//  5 4 3 2 1
+//  5 4 3 2 1
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int row, col;
+//     for (row = 1; row <= 5; row++)
+//     {
+//         for (col = 5; col >= 1; col--)
+//         {
+//             cout << col << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// 1 4 9 16 25
+// 1 4 9 16 25
+// 1 4 9 16 25
+// 1 4 9 16 25
+// 1 4 9 16 25
+// #include <iostream>
+//     using namespace std;
+// int main()
+// {
+//     int row, col;
+//     for (row = 1; row <= 5; row++)
+//     {
+//         for (col = 1; col <= 5; col++)
+//         {
+//             cout << col * col << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// a a a a a
+// b b b b b
+// c c c c c
+// d d d d d
+// e e e e e
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int row, col;
+//     char ch = 'a';
+//     for (row = 1; row <= 5; row++)
+//     {
+//         for (col = 1; col <= 5; col++)
+//         {
+//             cout << ch << " ";
+//         }
+//         cout << endl;
+//         ch = ch + 1;
+//     }
+// }
+
+//  a b c d e
+//  a b c d e
+//  a b c d e
+//  a b c d e
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int row;
+//     for (row = 1; row <= 5; row++)
+//     {
+//         for (char col = 'a'; col <= 'e'; col++)
+//         {
+//             cout << col << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+// Another Method
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int row, col;
+//     for (row = 1; row <= 5; row++)
+//     {
+//         for (col = 1; col <= 5; col++)
+//         {
+//             char name = 'a' + col - 1;
+//             cout << name << " ";
+//         }
+//         cout << endl;
+//     }
 // };
 
+//  1 2 3 4 5
+//  6 7 8 9 10
+//  11 12 13 14 15
+//  16 17 18 19 20
+//  21 22 23 24 25
 // #include <iostream>
 // using namespace std;
 // int main()
 // {
-//     char name;
-//     cout << "Printing Characters from 'a' to 'z': " << endl;
-//     for (name = 'a'; name <= 'z'; name++)
-//         cout << name << endl;
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int n = 10;
-//     cout << "Printing Number from 10 to 1: \n";
-//     for (n; n >= 1; n--)
-//         cout << n << endl;
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     cout << "Printing number from 1 to 100 with gaps of 3 in them: \n";
-//     for (int num = 1; num <= 100; num = num + 3)
-//         cout << num << endl;
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int table;
-//     cout << "Enter the number to print its table: ";
-//     cin >> table;
-//     for (int count = 1; count <= 10; count++)
-//         cout << table << " * " << count << " = " << table * count << endl;
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int num;
-//     cout << "Enter the number to print its table(in other way): ";
-//     cin >> num;
-//     for (int i = num; i <= num * 10; i = i + num)
-//         cout << i << endl;
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int num, power, answer = 1;
-//     cout << "Enter the number: ";
-//     cin >> num;
-//     cout << "Enter it's power: ";
-//     cin >> power;
-//     for (int i = 1; i <= power; i++)
-//         answer = answer * num;
-//     cout << num << " to the power " << power << " is: " << answer;
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int num, sum = 0;
-//     cout << "Enter the number to print its sum: ";
-//     cin >> num;
-//     for (int i = 1; i <= num; i++)
-//         sum = sum + i;
-//     cout << "Sum till " << num << " is: " << sum;
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int num;
-//     long long fact = 1;
-//     cout << "Enter the number to find its factorial: ";
-//     cin >> num;
-//     for (int i = 1; i <= num; i++)
-//         fact = fact * i;
-//     cout << "Factorial of " << num << " is: " << fact << endl;
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int p_check;
-//     cout << "Enter the number to check Prime or Not: ";
-//     cin >> p_check;
-//     if (p_check < 2)
+//     int row, col;
+//     int count = 1;
+//     for (row = 1; row <= 5; row++)
 //     {
-//         cout << "Not a Prime Number";
-//         return 0;
-//     }
-//     else
-//     {
-//         for (int count = 2; count < p_check; count++)
+//         for (col = 1; col <= 5; col++)
 //         {
-//             if (p_check % count == 0)
-//             {
-//                 cout << "Not a Prime Number";
-//                 return 0;
-//             }
+//             cout << count << " ";
+//             count++;
 //         }
-//         cout << "It's a Prime Number";
-//     }
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int num, fib, first = 0, second = 1;
-//     cout << "Enter the terms to print Fibonacci Series: ";
-//     cin >> num;
-//     if (num == 1)
-//         cout << "0";
-//     else if (num == 2)
-//         cout << "0" << endl
-//              << "1";
-//     else
-//     {
-//         cout << "0" << endl
-//              << "1\n";
-//         for (int i = 1; i < num - 1; i++)
-//         {
-//             fib = first + second;
-//             first = second;
-//             second = fib;
-//             cout << fib << endl;
-//         }
+//         cout << endl;
 //     }
 // };
 
-//<<<<<<<<<<<<<<<<<<<<<<HOMEWORK>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//<<<<<<<<<<<<<<<<<<<HOMEWORK>>>>>>>>>>>>>>>>>>>>>>>
+
+// 4 4 4 4 4 4
+// 4 4 4 4 4 4
+// 4 4 4 4 4 4
+// 4 4 4 4 4 4
+// 4 4 4 4 4 4
 
 // #include <iostream>
 // using namespace std;
 // int main()
 // {
-//     int num;
-//     cout << "Printing Number from 250 to 280 with the help of 'for' loop: \n";
-//     for (num = 250; num <= 280; num++)
-//         cout << num << endl;
+//     int row, col;
+//     for (row = 1; row <= 5; row++)
+//     {
+//         for (col = 1; col <= 6; col++)
+//             cout << "4 ";
+//         cout << endl;
+//     }
 // }
 
+// 1 8 27 64 125 216
+// 1 8 27 64 125 216
+// 1 8 27 64 125 216
+// 1 8 27 64 125 216
+// 1 8 27 64 125 216
 // #include <iostream>
 // using namespace std;
 // int main()
 // {
-//     char c = 'A';
-//     cout << "Printing Character from A to Z with the help of 'for' loop: \n";
-//     for (c; c <= 'Z'; c++)
-//         cout << c << endl;
+//     int row, col;
+//     for (row = 1; row <= 5; row++)
+//     {
+//         for (col = 1; col <= 6; col++)
+//         {
+//             cout << col * col * col << " ";
+//         }
+//         cout << endl;
+//     }
 // }
 
+// F G H I J K
+// F G H I J K
+// F G H I J K
+// F G H I J K
+// F G H I J K
 // #include <iostream>
 // using namespace std;
 // int main()
 // {
-//     char c = 'Z';
-//     cout << "Printing character from Z to A with the help of 'for' loop: \n";
-//     for (c; c >= 'A'; c--)
-//         cout << c << endl;
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int num = 220;
-//     cout << "There is an Arithmetic Progression, First number is 220 and Common Difference is 7. So printing all the numbers from 220 to 730: \n";
-//     for (num; num <= 720; num = num + 7)
-//         cout << num << endl;
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int num;
-//     long long sum = 0;
-//     cout << "Enter the number to Print it's Sum of Squares till the number: ";
-//     cin >> num;
-//     for (int i = 1; i <= num; i++)
-//         sum = sum + (i * i);
-//     cout << "Your sum is: " << sum;
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int num;
-//     long long sum = 0;
-//     cout << "Enter the number to Print it's Sum of Cube till the number: ";
-//     cin >> num;
-//     for (int i = 1; i <= num; i++)
-//         sum = sum + (i * i * i);
-//     cout << "Your sum is: " << sum;
+//     int row, col;
+//     for (row = 1; row <= 5; row++)
+//     {
+//         char ch = 'F';
+//         for (col = 1; col <= 6; col++)
+//         {
+//             cout << ch << " ";
+//             ch++;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
 // };
--- README.md
+++ README.md
@@ -1,10 +1,63 @@
-Today is Day 5 and today I learned about
+Today is Day 8 and today I learned about Pattern Problems
 Today's HW is:
+4 4 4 4 4 4
+4 4 4 4 4 4
+4 4 4 4 4 4
+4 4 4 4 4 4
+4 4 4 4 4 4
 
-1. Print number from 280 to 250 with the help of for loop.
-2. Print char from ‘A’ to ‘Z’ with the help of a for loop.
-3. Print char from ‘Z’ to ‘A’ with the help of a for loop.
-4. There is an Arithmetic Progression, First number is 220 and Common Difference is 7. So print all the numbers from 220 to 730 which follow the AP.
-5. Print Sum of square of first n natural number.
-6. Print Sum of cube of first n natural number
-7. Print n’th Fibonacci number.
+Second Pattern:
+
+       1 4 9 16 25
+       1 4 9 16 25
+       1 4 9 16 25
+       1 4 9 16 25
+       1 4 9 16 25
+       1 4 9 16 25
+
+Third Pattern:
+
+    1 8 27 64 125 216
+    1 8 27 64 125 216
+    1 8 27 64 125 216
+    1 8 27 64 125 216
+    1 8 27 64 125 216
+
+Fourth Pattern:
+
+F G H I J K  
+F G H I J K
+F G H I J K
+F G H I J K
+F G H I J K
+
+QUESTION: F G H I J K In this question if we solve like the above method...why answer comes in int?
+F G H I J K
+F G H I J K
+F G H I J K
+F G H I J K
+
+SOl.
+#include <iostream>
+using namespace std;
+int main()
+{
+int row, col;
+for (row = 1; row <= 5; row++)
+{
+char ch = 'F';
+for (col = 1; col <= 6; col++)
+{
+cout << ch+col-1 << " ";
+}
+cout << endl;
+}
+return 0;
+};
+
+OUTPUT:
+70 71 72 73 74 75
+70 71 72 73 74 75
+70 71 72 73 74 75
+70 71 72 73 74 75
+70 71 72 73 74 75
