c0726aa341ef66e0d60668585b6f566b8bc9ccce
Yeyaansh Patel <yeyaansh@gmail.com>
2023-09-26 22:38:41 +0530

    DAY-9

--- 180_days_challenge.cpp
+++ 180_days_challenge.cpp
@@ -1,8 +1,8 @@
-//  * * * * *
-//  * * * * *
-//  * * * * *
-//  * * * * *
-//  * * * * *
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
 // #include <iostream>
 // using namespace std;
 // int main()
@@ -10,45 +10,47 @@
 //     int row, col;
 //     for (row = 1; row <= 5; row++)
 //     {
-//         for (col = 1; col <= 5; col++)
+//         for (col = 1; col <= row; col++)
 //         {
-//             cout << " * ";
+//             cout << "* ";
 //         }
 //         cout << endl;
 //     }
 // }
 
-//  10 10 10 10 10
-//  10 10 10 10 10
-//  10 10 10 10 10
-//  10 10 10 10 10
-//  10 10 10 10 10
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
 // #include <iostream>
 // using namespace std;
 // int main()
 // {
-//     for (int row = 1; row <= 4; row++)
+//     int row, col;
+//     for (row = 1; row <= 5; row++)
 //     {
-//         for (int col = 1; col <= 5; col++)
+//         for (col = 1; col <= row; col++)
 //         {
-//             cout << "10 ";
+//             cout << col << " ";
 //         }
 //         cout << endl;
 //     }
 // }
 
-//  1 1 1 1 1
-//  2 2 2 2 2
-//  3 3 3 3 3
-//  4 4 4 4 4
-//  5 5 5 5 5
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
 // #include <iostream>
 // using namespace std;
 // int main()
 // {
-//     for (int row = 1; row <= 5; row++)
+//     int row, col;
+//     for (row = 1; row <= 5; row++)
 //     {
-//         for (int col = 1; col <= 5; col++)
+//         for (col = 1; col <= row; col++)
 //         {
 //             cout << row << " ";
 //         }
@@ -56,30 +58,11 @@
 //     }
 // }
 
-//  1 2 3 4 5
-//  1 2 3 4 5
-//  1 2 3 4 5
-//  1 2 3 4 5
-//  1 2 3 4 5
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     for (int row = 1; row <= 5; row++)
-//     {
-//         for (int col = 1; col <= 5; col++)
-//         {
-//             cout << col << " ";
-//         }
-//         cout << endl;
-//     }
-// }
-
-//  5 4 3 2 1
-//  5 4 3 2 1
-//  5 4 3 2 1
-//  5 4 3 2 1
-//  5 4 3 2 1
+// 1
+// 2 1
+// 3 2 1
+// 4 3 2 1
+// 5 4 3 2 1
 // #include <iostream>
 // using namespace std;
 // int main()
@@ -87,7 +70,22 @@
 //     int row, col;
 //     for (row = 1; row <= 5; row++)
 //     {
-//         for (col = 5; col >= 1; col--)
+//         for (col = 1; col <= row; col++)
+//         {
+//             cout << row - col + 1 << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+// <<<<<<<<<<<<<<ANOTHER METHOD>>>>>>>>>>>>>>>>>
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int row, col;
+//     for (row = 1; row <= 5; row++)
+//     {
+//         for (col = row; col >= 1; col--)
 //         {
 //             cout << col << " ";
 //         }
@@ -95,30 +93,10 @@
 //     }
 // }
 
-// 1 4 9 16 25
-// 1 4 9 16 25
-// 1 4 9 16 25
-// 1 4 9 16 25
-// 1 4 9 16 25
-// #include <iostream>
-//     using namespace std;
-// int main()
-// {
-//     int row, col;
-//     for (row = 1; row <= 5; row++)
-//     {
-//         for (col = 1; col <= 5; col++)
-//         {
-//             cout << col * col << " ";
-//         }
-//         cout << endl;
-//     }
-// }
-
-// a a a a a
-// b b b b b
-// c c c c c
-// d d d d d
+// a
+// b b
+// c c c
+// d d d d
 // e e e e e
 // #include <iostream>
 // using namespace std;
@@ -128,118 +106,117 @@
 //     char ch = 'a';
 //     for (row = 1; row <= 5; row++)
 //     {
-//         for (col = 1; col <= 5; col++)
+//         for (col = 1; col <= row; col++)
 //         {
 //             cout << ch << " ";
 //         }
+//         ch++;
 //         cout << endl;
-//         ch = ch + 1;
 //     }
 // }
-
-//  a b c d e
-//  a b c d e
-//  a b c d e
-//  a b c d e
+// <<<<<<<<<<<<<<<<<<<<ANOTHER METHOD>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 // #include <iostream>
 // using namespace std;
 // int main()
 // {
-//     int row;
+//     int row, col;
 //     for (row = 1; row <= 5; row++)
 //     {
-//         for (char col = 'a'; col <= 'e'; col++)
+//         char name = 'a' + row - 1;
+//         for (col = 1; col <= row; col++)
+//         {
+//             cout << name << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     for (int row = 1; row <= 5; row++)
+//     {
+//         for (int col = 1; col <= 5 - row + 1; col++)
+//         {
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int row, col;
+//     for (row = 5; row >= 1; row--)
+//     {
+//         for (col = 1; col <= row; col++)
 //         {
 //             cout << col << " ";
 //         }
 //         cout << endl;
 //     }
 // }
-// Another Method
+
+// 5
+// 5 4
+// 5 4 3
+// 5 4 3 2
+// 5 4 3 2 1
 // #include <iostream>
 // using namespace std;
 // int main()
 // {
 //     int row, col;
-//     for (row = 1; row <= 5; row++)
+//     for (row = 5; row >= 1; row--)
 //     {
-//         for (col = 1; col <= 5; col++)
+//         for (col = 5; col >= row; col--)
 //         {
-//             char name = 'a' + col - 1;
-//             cout << name << " ";
+//             cout << col << " ";
 //         }
 //         cout << endl;
 //     }
 // };
 
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<HOMEWORK>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//  1
+//  1 2
+//  1 2 3
+//  1 2 3 4
 //  1 2 3 4 5
-//  6 7 8 9 10
-//  11 12 13 14 15
-//  16 17 18 19 20
-//  21 22 23 24 25
+//  1 2 3 4 5 6
 // #include <iostream>
 // using namespace std;
 // int main()
 // {
 //     int row, col;
-//     int count = 1;
-//     for (row = 1; row <= 5; row++)
+//     for (row = 1; row <= 6; row++)
 //     {
-//         for (col = 1; col <= 5; col++)
+//         for (col = 1; col <= row; col++)
 //         {
-//             cout << count << " ";
-//             count++;
-//         }
-//         cout << endl;
-//     }
-// };
-
-//<<<<<<<<<<<<<<<<<<<HOMEWORK>>>>>>>>>>>>>>>>>>>>>>>
-
-// 4 4 4 4 4 4
-// 4 4 4 4 4 4
-// 4 4 4 4 4 4
-// 4 4 4 4 4 4
-// 4 4 4 4 4 4
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int row, col;
-//     for (row = 1; row <= 5; row++)
-//     {
-//         for (col = 1; col <= 6; col++)
-//             cout << "4 ";
-//         cout << endl;
-//     }
-// }
-
-// 1 8 27 64 125 216
-// 1 8 27 64 125 216
-// 1 8 27 64 125 216
-// 1 8 27 64 125 216
-// 1 8 27 64 125 216
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int row, col;
-//     for (row = 1; row <= 5; row++)
-//     {
-//         for (col = 1; col <= 6; col++)
-//         {
-//             cout << col * col * col << " ";
+//             cout << col << " ";
 //         }
 //         cout << endl;
 //     }
 // }
 
-// F G H I J K
-// F G H I J K
-// F G H I J K
-// F G H I J K
-// F G H I J K
+// A
+// A B
+// A B C
+// A B C D
+// A B C D E
 // #include <iostream>
 // using namespace std;
 // int main()
@@ -247,13 +224,57 @@
 //     int row, col;
 //     for (row = 1; row <= 5; row++)
 //     {
-//         char ch = 'F';
-//         for (col = 1; col <= 6; col++)
+//         char ch = 'A';
+
+//         for (col = 1; col <= row; col++)
 //         {
 //             cout << ch << " ";
 //             ch++;
 //         }
 //         cout << endl;
 //     }
-//     return 0;
+// }
+
+//    10
+//    10 11
+//    10 11 12
+//    10 11 12 13
+//    10 11 12 13 14
+//    10 11 12 13 14 15
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int row, col;
+//     for (row = 1; row <= 6; row++)
+//     {
+//         int num = 10;
+//         for (col = 1; col <= row; col++)
+//         {
+//             cout << num << " ";
+//             num++;
+//         }
+//         cout << endl;
+//     }
+// }
+
+//   A B C D
+//   A B C
+//   A B
+//   A
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int row, col;
+//     for (row = 1; row <= 5; row++)
+//     {
+//         char ch = 'A';
+//         for (col = 1; col <= 5 - row; col++)
+//         {
+//             cout << ch << " ";
+//             ch++;
+//         }
+//         cout << endl;
+//     }
 // };
--- README.md
+++ README.md
@@ -1,63 +1,34 @@
-Today is Day 8 and today I learned about Pattern Problems
+Today is Day 9 and today I learned about Pattern-Printing-2 Problems
 Today's HW is:
-4 4 4 4 4 4
-4 4 4 4 4 4
-4 4 4 4 4 4
-4 4 4 4 4 4
-4 4 4 4 4 4
+First Pattern:
+
+     1
+     1 2
+     1 2 3
+     1 2 3 4
+     1 2 3 4 5
+     1 2 3 4 5 6
 
 Second Pattern:
 
-       1 4 9 16 25
-       1 4 9 16 25
-       1 4 9 16 25
-       1 4 9 16 25
-       1 4 9 16 25
-       1 4 9 16 25
+    A
+    A B
+    A B C
+    A B C D
+    A B C D E
 
 Third Pattern:
 
-    1 8 27 64 125 216
-    1 8 27 64 125 216
-    1 8 27 64 125 216
-    1 8 27 64 125 216
-    1 8 27 64 125 216
+       10
+       10 11
+       10 11 12
+       10 11 12 13
+       10 11 12 13 14
+       10 11 12 13 14 15
 
 Fourth Pattern:
 
-F G H I J K  
-F G H I J K
-F G H I J K
-F G H I J K
-F G H I J K
-
-QUESTION: F G H I J K In this question if we solve like the above method...why answer comes in int?
-F G H I J K
-F G H I J K
-F G H I J K
-F G H I J K
-
-SOl.
-#include <iostream>
-using namespace std;
-int main()
-{
-int row, col;
-for (row = 1; row <= 5; row++)
-{
-char ch = 'F';
-for (col = 1; col <= 6; col++)
-{
-cout << ch+col-1 << " ";
-}
-cout << endl;
-}
-return 0;
-};
-
-OUTPUT:
-70 71 72 73 74 75
-70 71 72 73 74 75
-70 71 72 73 74 75
-70 71 72 73 74 75
-70 71 72 73 74 75
+      A B C D
+      A B C
+      A B
+      A
