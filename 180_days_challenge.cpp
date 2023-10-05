3813b5c48e2d7c8a03050360ecb81a9ea13904bc
Yeyaansh Patel <yeyaansh@gmail.com>
2023-10-05 18:29:24 +0530

    DAY-18

--- 180_days_challenge.cpp
+++ 180_days_challenge.cpp
@@ -1,153 +1,138 @@
-//<<<<<<<<<<<<<<<<<<<<<<<<        DAY-16      >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// #include <iostream>
+// using namespace std;
+
+// int sum(int x, int y) // function declaration
+// {
+//     int ans = x + y; // function define
+//     return ans;
+// }
+
+// int mul(int x, int y)
+// {
+//     int mul = 1;
+//     mul = x * y;
+//     return mul;
+// }
+
+// void fun()
+// {
+//     cout << "Hello Coder Army";
+// }
+// int main()
+// {
+//     int a, b;
+//     cout << "Enter the value of a and b to find it's sum and product using function: \n";
+//     cin >> a >> b;
+//     cout << "Sum of a and b using function is: " << sum(a, b) << endl;     // function call
+//     cout << "Product of a and b using function is: " << mul(a, b) << endl; // function call
+//     fun();
+// }
+
+// #include <iostream>
+// using namespace std;
+// bool isPrime(int x)
+// {
+//     if (x < 2)
+//         return 0;
+//     for (int i = 2; i < x; i++)
+//     {
+//         if (x % i == 0)
+//             return 0;
+//     }
+//     return 1;
+// }
+// int fact(int m)
+// {
+//     int fact = 1;
+//     for (int i = 2; i <= m; i++)
+//     {
+//         fact *= i;
+//     }
+//     return fact;
+// }
+// int sum(int x = 3, int y = 2) // function declaration
+// {
+//     int ans = x + y; // function define
+//     return ans;
+// }
+// void isPrimeNo(int x)
+// {
+//     if (x < 2)
+//     {
+//         cout << 0 << endl;
+//         return;
+//     }
+//     for (int i = 2; i < x; i++)
+//     {
+//         if (x % i == 0)
+//             cout << 0 << endl;
+//         return;
+//     }
+//     cout << 1 << endl;
+//     return;
+// }
+// int main()
+// {
+//     int a, b;
+//     cout << "Enter the value of a: " << endl;
+//     cin >> a;
+//     cout << "Enter the value of b: " << endl;
+//     cin >> b;
+//     cout << a << " is " << isPrime(a) << endl;
+//     cout << b << " factorial is: " << fact(a);
+//     cout << "Default parameter is: " << sum() << endl;
+//     isPrimeNo(a);
+// }
+
+// call by value or pass by value
+// #include <iostream>
+// using namespace std;
+
+// void swapit(int a, int b)
+// {
+//     int temp = a;
+//     a = b;
+//     b = temp;
+// }
+// int main()
+// {
+//     int a, b;
+//     cout << "Enter the value of a & b to swap: ";
+//     cin >> a >> b;
+//     swapit(a, b);
+//     cout << "a is now " << a << " ans b is now " << b;
+// }
+
+// call by reference or pass by reference
+// #include <iostream>
+// using namespace std;
+
+// void swapit(int &a, int &b)
+// {
+//     int temp = a;
+//     a = b;
+//     b = temp;
+// }
+// int main()
+// {
+//     int a, b;
+//     cout << "Enter the value of a & b to swap: ";
+//     cin >> a >> b;
+//     swapit(a, b);
+//     cout << "a is now " << a << " ans b is now " << b;
+// }
+
+// swap is an in-built function...
 // #include <iostream>
 // using namespace std;
 // int main()
 // {
-//     int num, rem, ans = 0, mul = 1, store;
-//     cout << "Enter the number to convert it into binary: ";
-//     cin >> num;
-//     store = num;
-//     while (num > 0)
-//     {
-//         rem = num % 2;
-//         ans = rem * mul + ans;
-//         num /= 2;
-//         mul = mul * 10;
-//     }
-//     cout << "Binary of " << store << " is: " << ans;
-// }
-
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int num;
-//     cout << "Enter to convert binary into decimal: ";
-//     cin >> num;
-//     int ans = 0, mul = 1, rem;
-//     int store = num;
-//     while (num > 0)
-//     {
-//         rem = num % 10;
-//         ans = rem * mul + ans;
-//         num /= 10;
-//         mul *= 2;
-//     }
-//     cout << store << " in decimal form is " << ans;
-// }
-
-//<<<<<<<<<<<<<<<<<<<<<<<<        DAY-17      >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-// 1. Add Digits
-// https://leetcode.com/problems/add-digits/
-// class Solution {
-// public:
-//     int addDigits(int num) {
-
-//         while(num>9)
-//         {
-//         int ans = 0, rem;
-//         while(num)
-//         {
-//             rem = num%10;
-//             ans+= rem;
-//             num/=10;
-//         }
-//         num = ans;
-//         }
-//         return num;
-//     }
-// };
-
-// 2. Leap Year
-// https://practice.geeksforgeeks.org/problems/leap-year0943/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
-// int isLeap(int N){
-
-//     if(N%400==0)
-//     return 1;
-//     else if(N%4==0 && N%100!=0)
-//     return 1;
-//     else return 0;
-// }
-
-// 3. Reverse the number
-// https: // leetcode.com/problems/reverse-integer/
-// class Solution {
-// public:
-//     int reverse(int x) {
-//         int ans = 0;
-//         while(x)
-//         {
-//             if(ans>INT_MAX/10 || ans<INT_MIN/10)
-//             return 0;
-//             ans = (x%10) + ans*10;
-//             x/=10;
-
-//         }
-//         return ans;
-
-//     }
-// };
-
-// 4. Power of Two(2)
-//     https: // leetcode.com/problems/power-of-two/description/
-// class Solution {
-// public:
-//     bool isPowerOfTwo(int n) {
-
-//         if(n<1)
-//         return 0;
-//         while(n!=1)
-//         {
-//             if(n%2==1)
-//             return 0;
-//             n/=2;
-//         }
-//         return 1;
-//     }
-// };
-
-// 5. SqareRoot of x (HW)
-// https: // leetcode.com/problems/sqrtx/description/
-
-// 6. Palindrome
-// https://leetcode.com/problems/palindrome-number/description/
-// class Solution {
-// public:
-//     bool isPalindrome(int x) {
-//         if(x<0)
-//         return 0;
-//         int store=x, ans = 0, rem;
-//         while(store)
-//         {
-//             rem = store%10;
-//             store/=10;
-//             if(ans > INT_MAX/10 )
-//             return 0;
-//             ans = ans*10 + rem;
-//         }
-//         if(ans == x)
-//         return 1;
-//         else
-//         return 0;
-//     }
-// };
-
-// 7. Complement of Base 10 Integer
-// https://leetcode.com/problems/complement-of-base-10-integer/description/
-// class Solution {
-// public:
-//     int bitwiseComplement(int n) {
-//         if(n==0)
-//         return 1;
-//         int ans = 0, rem, mul = 1;
-//         while(n)
-//         {
-//             rem = n%2;
-//             rem = rem ^ 1;
-//             ans = ans + rem*mul;
-//             mul*=2;
-//             n/=2;
-//         }
-//         return ans;
-//     }
+//     int a, b;
+//     cout << "Enter the value of a & b to swap: ";
+//     cin >> a >> b;
+//     swap(a, b);
+//     cout << "a is now " << a << " ans b is now " << b << endl;
+//     float f1 = 23.43, f2 = 234.53;
+//     swap(f1, f2);
+//     cout << f1 << " " << f2;
 // };
--- README.md
+++ README.md
@@ -1,13 +1,15 @@
-Today is Day 16 & Day 17 and today I learned about conversion of decimal to biary and vice-versa and solved questions on leetcode and on geeksforgeeks
+Today is Day 18 and today I learned about functions and pass by value and pass by reference..
 Today's HW is:
 
-1. Write a program to convert binary numbers to decimal numbers using a for loop.
-2. Write a program to convert decimal numbers to binary numbers using a for loop.
-3. Write a program to convert decimal numbers to Octal numbers.
-4. Write a program to convert Octal numbers to decimal numbers.
-5. Write a program to convert binary to Octal numbers
-6. Write a program to convert Octal numbers to binary numbers
+1: Find the cube of a number using Function.
 
-7. https://leetcode.com/problems/sqrtx/
-8. https://leetcode.com/problems/ugly-number/
-9. https://practice.geeksforgeeks.org/problems/squares-in-nn-chessboard1801/1?page=1&difficulty[]=-1&difficulty[]=0&category[]=Numbers&category[]=number-theory&sortBy=submissions
+2: Reverse a number n using Function, Constraints: -5000<=n<=5000
+
+3: There are three numbers a,b,c. Put the value of a into b, put value of b into c and put value of c into a. Do it using Function.
+
+4: Swap 2 numbers a, b without using extra variables. Range of
+-10000<=a,b<=100000.
+
+5: Print “Hello Coder Army” n times using Function.
+
+6: Given two numbers n, r. Find nCr (Combination). Use Function here.
