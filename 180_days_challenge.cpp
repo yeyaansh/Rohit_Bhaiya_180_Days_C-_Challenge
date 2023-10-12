580ded44b10dd39f1fbd6d10753642a21fd7f2d5
Yeyaansh Patel <yeyaansh@gmail.com>
2023-10-12 19:59:13 +0530

    DAY-25

--- 180_days_challenge.cpp
+++ 180_days_challenge.cpp
@@ -1,150 +1,29 @@
-// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DAY-23>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-// 1. Search an element in an array
-// https://practice.geeksforgeeks.org/problems/search-an-element-in-an-array-1587115621/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
-// class Solution
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DAY-25>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// #include <iostream>
+// using namespace std;
+// int main()
 // {
-// public:
-//     // Function to search x in arr
-//     // arr: input array
-//     // X: element to be searched for
-//     int search(int arr[], int N, int X)
+//     int arr[9] = {2, 24, 12, 53, 12, 4321, 4, 511, 6};
+//     cout << "Sorting the given array using Selection Sorting\n";
+//     for (int i = 0; i < 8 /*n-1*/; i++)
 //     {
+//         int index = i;
+//         for (int j = i + 1; j < 9 /*n-1*/; j++)
 
-//         // Your code here
-//         for (int i = 0; i < N; i++)
 //         {
-//             if (arr[i] == X)
-//                 return i;
+//             if (arr[j] < arr[index])
+//             {
+//                 index = j;
+//             }
 //         }
-//         return -1;
+//         /*int temp = arr[j];
+//                 arr[j] = arr[i];
+//                 arr[i] = temp;
+//                 */
+//         swap(arr[i], arr[index]);
 //     }
-// };
-
-// Reverse an Array
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int arr[6] = {1, 2, 4, 5, 6, 7};
-//     int start = 0, end = 5;
-//     while (start < end)
+//     for (int i = 0; i < 9 /*n*/; i++)
 //     {
-//         swap(arr[start], arr[end]);
-//         start++;
-//         end--;
-//     }
-//     for (int i = 0; i < 6; i++)
 //         cout << arr[i] << " ";
-// }
-
-// 2. Second Max
-// https://practice.geeksforgeeks.org/problems/second-largest3735/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
-// class Solution{
-// public:
-// 	// Function returns the second
-// 	// largest elements
-// 	int print2largest(int arr[], int n) {
-// 	    // code here
-// 	    int lar=INT_MIN;
-// 	    for(int i=0;i<n;i++)
-// 	    {
-// 	        if(arr[i]>lar)
-// 	        {
-// 	            lar = arr[i];
-// 	        }
-// 	    }
-// 	    int slar = -1;
-// 	    for(int i=0;i<n;i++)
-// 	    {
-// 	        if(arr[i]!=lar)
-// 	        {
-// 	           if(arr[i]>slar)
-// 	           {
-// 	               slar = arr[i];
-// 	           }
-// 	        }
-// 	    }
-// 	    return slar;
-// 	}
-// };
-
-// <<<<<<<<<<<<   OR    >>>>>>>>>>>>>>>>>>>>
-
-// class Solution{
-// public:
-// 	// Function returns the second
-// 	// largest elements
-// 	int print2largest(int arr[], int n) {
-// 	    // code here
-// 	    int lar=INT_MIN;
-// 	    for(int i=0;i<n;i++)
-// 	    {
-// 	        if(arr[i]>lar)
-// 	        {
-// 	            lar = arr[i];
-// 	        }
-// 	    }
-// 	    int slar = -1;
-// 	    for(int i=0;i<n;i++)
-// 	    {
-// 	        if(arr[i]!=lar)
-// 	        {
-// 	           slar = max(arr[i],slar);
-// 	        }
-// 	    }
-// 	    return slar;
-// 	}
-// };
-
-// 3. Missing Number
-// https://practice.geeksforgeeks.org/problems/missing-number-in-array1416/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
-// class Solution{
-//   public:
-//     int missingNumber(vector<int>& array, int n) {
-//       int sum =0;
-//       for(int i=0;i<n-1;i++)
-//       {
-//           sum = sum + array[i];
-
-//       }
-//       int ans = n*(n+1)/2;
-//       return ans-sum;
 //     }
-// };
-
-// Fibonacci Series
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int n;
-//     cout << "Enter the number: ";
-//     cin >> n;
-//     int arr[100];
-//     arr[0] = 0;
-//     arr[1] = 1;
-//     for (int i = 2; i < n; i++)
-//     {
-//         arr[i] = arr[i - 1] + arr[i - 2];
-//     }
-//     cout << arr[n - 1];
 // }
-
-// 4. Rotate Array by 1
-// https://practice.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one2614/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
-// void rotate(int arr[], int n)
-// {
-
-//     int temp = arr[n];
-//     for(int i=n;i>=1;i--)
-//     {
-//         arr[i] = arr[i-1];
-//     }
-//     arr[0] = temp;
-
-// }
-
-// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DAY-24>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-//  TIME COMPLEXITY AND SPACE COMPLEXITY
--- README.md
+++ README.md
@@ -1,62 +1,10 @@
-Today is Day 23 & Day-24 and Today I learned about questions on array and time & space complexity..
+Today is Day 25 and Today I learned about Sorting (selection sorting)..
 Today's HW is:
 
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DAY-23>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DAY-25>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-1. Smallest Positive missing number
-   // https://practice.geeksforgeeks.org/problems/smallest-positive-missing-number-1587115621/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
+1: Use a Selection Sort Algorithm to sort the array of integers in decreasing order.
 
-2. Move all negative elements to end
-   // https://practice.geeksforgeeks.org/problems/move-all-negative-elements-to-end1813/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
+2: Use a Selection Sort Algorithm to sort the array of integers in increasing order by taking the highest number to last place. Question was explained in the class.
 
-3. Number of occurrence
-   // https://practice.geeksforgeeks.org/problems/number-of-occurrence2259/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
-
-4. Count number of elements between two given elements in array
-   // https://practice.geeksforgeeks.org/problems/count-number-of-elements-between-two-given-elements-in-array4044/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
-
-5. First Repeating Element
-   // https://practice.geeksforgeeks.org/problems/first-repeating-element4018/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
-
-6. Sum of Unique Elements
-   // https://leetcode.com/problems/sum-of-unique-elements/
-
-Hi Students, if you are a beginner, maybe you won’t be able to solve all the problems, so it's totally ok, I will cover some of the questions in the upcoming class. So till then keep going.
-
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DAY-24>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-Find the Time Complexity Big(O), Theta, Omega:
-
-for(int i=1;i<=n\*n;i++)
-cout<<"Coder Army";
-
-2: for(int i=1;i<=n\*n;i=i+2)
-cout<<"Coder Army";
-
-3: for(int i=1;i<=n;i++)
-for(int j=1;j<=n;j=j+5)
-cout<<"Coder Army";
-
-4: for(int i=1;i<=n;i++)
-for(int j=i;j<=n;j=j++)
-cout<<"Coder Army";
-
-5: for(int i=1;i<=n;i++)
-for(int j=1;j<=n;j=j\*4)
-cout<<"Coder Army";
-
-6: for(int i=1;i<=n;i=i\*2)
-for(int j=1;j<=i;j=j++)
-cout<<"Coder Army";
-
-7: for(int i=1;i<=n;i++)
-for(int j=1;j<=n;j=j++)
-for(int k=1;k<=n;k=k\*3)
-cout<<"Coder Army";
-
-8: for(int i=1;i<=n;i++)
-for(int j=1;j<=n;j=j++)
-for(int k=1;k<=n;k++)
-cout<<"Coder Army";
-
-Learn more about the Auxiliary Space Complexity and Total Space Complexity.
+3: Use a Selection Sort Algorithm to sort the array of char in ascending order.
