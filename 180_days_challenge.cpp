7886a46faba894437a010925d064ee8c15571389
Yeyaansh Patel <yeyaansh@gmail.com>
2023-10-24 13:42:39 +0530

    DAY-30 & DAY-31

--- 180_days_challenge.cpp
+++ 180_days_challenge.cpp
@@ -1,111 +1,253 @@
-// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DAY-26>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DAY-30>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 // #include <iostream>
 // using namespace std;
 // int main()
 // {
-//     int arr[100], n;
-//     printf("Enter the Size of Array: ");
-//     cin >> n;
-//     if (n > 100)
-//         return 0;
-//     for (int i = 0; i < n; i++)
-//         cin >> arr[i];
-//     for (int i = 0; i < n - 1; i++)
+//     int arr[] = {2, 4, 67, 542, 3232, 43434};
+//     cout << "Entered Number is '67', which is to be searched\n";
+//     int start = 0, end = 5, key = 67;
+//     int mid = (start + end) / 2;
+//     while (start <= end)
 //     {
-//         for (int j = 0; j < n - 1 - i; j++)
+//         if (arr[mid] == key)
 //         {
-//             if (arr[j] > arr[j + 1])
-//                 swap(arr[j], arr[j + 1]);
+//             cout << "Got it! at positon: " << mid;
+//             return 0;
 //         }
+//         else if (arr[mid] < key)
+//             start = mid + 1;
+//         else
+//             end = mid - 1;
 //     }
-//     cout << "Entered Array is now being sorted using Bubble Sorting: \n";
-//     for (int i = 0; i < n; i++)
-//         cout << arr[i] << " ";
 // }
 
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< OR >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// <<<<<<<<<<<<<<<<<<<<<<<<< OR >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 // #include <iostream>
 // using namespace std;
-// int main()
+
+// int binarySearch(int arr[], int n, int key)
 // {
-//     int arr[100], n;
-//     printf("Enter the Size of Array: ");
-//     cin >> n;
-//     if (n > 100)
-//         return 0;
-//     for (int i = 0; i < n; i++)
-//         cin >> arr[i];
-//     for (int i = n - 2; i >= 0; i--)
+//     int start = 0, end = n - 1, mid;
+//     while (start <= end)
 //     {
-//         for (int j = 0; j <= i; j++)
-//         {
-//             if (arr[j] > arr[j + 1])
-//                 swap(arr[j], arr[j + 1]);
-//         }
+//         mid = (start + end) / 2;
+
+//         if (arr[mid] == key)
+//             return mid;
+
+//         else if (arr[mid] < key)
+//             start = mid + 1;
+
+//         else
+//             end = mid - 1;
 //     }
-//     cout << "Entered Array is now being sorted using Bubble Sorting: \n";
-//     for (int i = 0; i < n; i++)
-//         cout << arr[i] << " ";
+
+//     return -1;
 // }
 
-// <<<<<<<<<<<<<<<< For Best Case Scenario (array is sorted)>>>>>>>>>>>>>>>>>>>>>>>>>>
-// #include <iostream>
-// using namespace std;
 // int main()
 // {
-//     int arr[100], n;
-//     printf("Enter the Size of Array: ");
+//     int arr[1000];
+//     cout << "Enter the size of your array: ";
+//     int n;
 //     cin >> n;
-//     if (n > 100)
-//         return 0;
+//     cout << "Enter the elements of your array\n";
 //     for (int i = 0; i < n; i++)
 //         cin >> arr[i];
-//     for (int i = 0; i < n - 1; i++)
-//     {
-//         bool swapped = 0;
-//         for (int j = 0; j < n - 1 - i; j++)
-//         {
-//             if (arr[j] > arr[j + 1])
-//             {
-//                 swap(arr[j], arr[j + 1]);
-//                 swapped = 1;
-//             }
-//         }
-//         if (swapped == 0)
-//         {
-//             cout << "Your array is already sorted: \n";
-//             break;
-//         }
-//     }
-//     cout << "Entered Array is now being sorted using Bubble Sorting: \n";
-//     for (int i = 0; i < n; i++)
-//         cout << arr[i] << " ";
+//     cout << "Enter the element you want to find: ";
+//     int key;
+//     cin >> key;
+//     cout << binarySearch(arr, n, key);
 // }
 
-// Bubble Sorting
-// https://practice.geeksforgeeks.org/problems/bubble-sort/1
+// Binary Search
+// https://leetcode.com/problems/binary-search/
+
 // class Solution
 // {
-//     public:
-//     //Function to sort the array using bubble sort algorithm.
-//     void bubbleSort(int arr[], int n)
+// public:
+//     int search(vector<int> &nums, int target)
 //     {
-//         // Your code here
-//         for (int i = 0; i < n - 1; i++)
-//     {
-//         bool swapped = 0;
-//         for (int j = 0; j < n - 1 - i; j++)
+
+//         int n = nums.size();
+
+//         int start = 0, end = n - 1, mid;
+//         while (start <= end)
 //         {
-//             if (arr[j] > arr[j + 1])
+//             mid = (start + end) / 2;
+//             if (nums[mid] == target)
+//                 return mid;
+//             else if (nums[mid] < target)
+//                 start = mid + 1;
+//             else
+//                 end = mid - 1;
+//         }
+//         return -1;
+//     }
+// };
+
+// <<<<<<<<<<<< To stop the overflow condition >>>>>>>>>>>>>>>>>>>>>>>
+// class Solution
+// {
+// public:
+//     int search(vector<int> &nums, int target)
+//     {
+
+//         int n = nums.size();
+
+//         int start = 0, end = n - 1, mid;
+//         while (start <= end)
+//         {
+//             mid = start + (end-start)/2; // for overflow condition
+//             if (nums[mid] == target)
+//                 return mid;
+//             else if (nums[mid] < target)
+//                 start = mid + 1;
+//             else
+//                 end = mid - 1;
+//         }
+//         return -1;
+//     }
+// };
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DAY-31>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// 1 : Find First and Last Position of Element in Sorted Array
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+// class Solution {
+// public:
+//     vector<int> searchRange(vector<int>& nums, int target) {
+
+//         int start = 0, end = nums.size()-1, mid, first = -1, last = -1;
+
+// // First Element
+
+//         while(start<=end)
+//         {
+//             mid = start + (end-start)/2;
+//             if(nums[mid] == target)
 //             {
-//                 swap(arr[j], arr[j + 1]);
-//                 swapped = 1;
+//                 first = mid;
+//                 end = mid-1;
+//             }
+//             else if(nums[mid]<target)
+//             {
+//                 start = mid + 1;
+//             }
+//             else
+//             end = mid -1;
+//         }
+
+// start = 0, end = nums.size()-1;
+
+// // Last Element
+
+//              while(start<=end)
+//         {
+//             mid = start + (end-start)/2;
+//             if(nums[mid] == target)
+//             {
+//                 last = mid;
+//                 start = mid+1;
+//             }
+//             else if(nums[mid]<target)
+//             {
+//                 start = mid + 1;
+//             }
+//             else
+//             end = mid -1;
+//         }
+
+//         // Create an array to return the value
+
+//     vector<int>a(2);
+//     a[0]=first;
+//     a[1] = last;
+// return a;
+
+//     }
+
+// };
+
+// 2: Search Insert Position
+// https://leetcode.com/problems/search-insert-position/description/
+// <<<<<<<<<<<<<<<<<<<<<<<<<< By Me >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         int mid, start = 0, end = nums.size()-1, index;
+//         while(start<=end)
+//         {
+//             mid = start + (end-start)/2;
+//             if(nums[mid] == target)
+//             {
+//                 index = mid;
+//                 return mid;
+//             }
+//             else if(nums[mid] < target)
+//             {
+//             start = mid + 1;
+//             index = mid+1;
+//             }
+//             else {
+//             end = mid -1;
+//             index = mid;
 //             }
 //         }
-//         if (swapped == 0)
-//         {
-//             break;
-//         }
+//         return index;
 //     }
+// };
+
+// <<<<<<<<<<<<<<<<<<<<<<<< By Rohit Bhaiya (most optimized)>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& arr, int target) {
+//         int start=0, end = arr.size()-1, mid, ans = arr.size();
+//         while(start <= end)
+//         {
+//             mid = start + (end-start)/2;
+//             if(arr[mid]==target)
+//             {
+//                 return mid;
+//             }
+//             else if( arr[mid]< target)
+//             start = mid + 1;
+//             else
+//             {
+//                 ans = mid;
+//                 end = mid-1;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+// 3: Sqrt(x)
+// https://leetcode.com/problems/sqrtx/description/
+// class Solution {
+// public:
+//     int mySqrt(int x) {
+//         if(x<2)
+//         return x;
+//         int start = 0, end = x, mid, ans;
+//         while(start<=end)
+//         {
+//             mid = start + (end-start)/2;
+//             if(mid == x/mid)
+//             {
+//                 ans = mid;
+//                 break;
+//             }
+//             else if(mid < x/mid)
+//             {
+//                 ans = mid;
+//                 start = mid + 1;
+//             }
+//             else
+//             end = mid-1;
+//         }
+//         return ans;
 //     }
 // };
--- README.md
+++ README.md
@@ -1,10 +1,34 @@
-Today is Day 25 and Today I learned about Sorting (bubble sorting)..
+Today is Day-30 & Day-31 and Today I learned about Questions on Binary Search..
 Today's HW is:
 
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DAY-26>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DAY-30>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-1: Use Bubble Sort Algorithm to sort the array of integers in decreasing order.
+1: An array is given in decreasing order with Key, Find the index of key, if key is not present, print -1;
 
-2: Bubble Sort Algorithm to sort the array of integers in increasing order by taking the smallest number to first place by starting from the last. Question was explained in the class.
+2: Search Insert Position
+https://leetcode.com/problems/search-insert-position/\
 
-3: Bubble Sort Algorithm to sort the array of char in ascending order.
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DAY-31>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+1: Find First and Last Position of Element in Sorted Array
+https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+
+2: Search Insert Position
+https://leetcode.com/problems/search-insert-position/description/
+
+3: Sqrt(x)
+https://leetcode.com/problems/sqrtx/description/
+
+4: Kth Missing Positive Number
+https://leetcode.com/problems/kth-missing-positive-number/description/
+
+5: Count the Zeros
+https://practice.geeksforgeeks.org/problems/count-the-zeros2550/1?page=1&difficulty[]=0&category[]=Searching&sortBy=submissions
+
+6:Number of occurrence
+https://practice.geeksforgeeks.org/problems/number-of-occurrence2259/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
+
+7: Cube root of a number
+https://practice.geeksforgeeks.org/problems/cube-root-of-a-number0915/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
+
+Learn About Lower bound and Upper bound in sorted arrays.
