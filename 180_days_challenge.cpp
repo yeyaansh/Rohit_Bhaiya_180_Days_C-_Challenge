3c1e2d34f604257acd8b54f2d745307c04b41f40
Yeyaansh Patel <yeyaansh@gmail.com>
2023-11-13 16:07:08 +0530

    DAY-40 and DAY-43

--- 180_days_challenge.cpp
+++ 180_days_challenge.cpp
@@ -1,55 +1,183 @@
-// #include <iostream>
-// #include <algorithm>
-// #include <climits>
-// #include <vector>
-// using namespace std;
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<   DAY-40    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-// bool divide(vector<int> arr)
+// 1. Trapping Rain Water
+// https://leetcode.com/problems/trapping-rain-water/submissions/1095093458/
+//     class Solution
 // {
-//     int max = INT_MIN, prefix = 0, total_sum = 0, n = arr.size();
-//     // total sum
-//     for (int i = 0; i < n; i++)
-//         total_sum += arr[i];
-
-//     for (int i = 0; i < n - 1; i++)
+// public:
+//     int trap(vector<int> &height)
 //     {
-//         prefix += arr[i];
-//         int ans = total_sum - prefix;
-//         if (ans == prefix)
-//             return 1;
+//         int n = height.size(), water = 0;
+//         int leftmax = 0, rightmax = 0, maxheight = INT_MIN, index = 0;
+
+//         // max height building
+//         for (int i = 0; i < n; i++)
+//         {
+//             if (maxheight < height[i])
+//             {
+//                 maxheight = height[i];
+//                 index = i;
+//             }
+//         }
+
+//         // left part
+//         for (int i = 0; i < index; i++)
+//         {
+//             if (leftmax > height[i])
+//             {
+//                 water += (leftmax - height[i]);
+//             }
+//             else
+//                 leftmax = height[i];
+//         }
+
+//         // right part
+//         for (int i = n - 1; i > index; i--)
+//         {
+//             if (rightmax > height[i])
+//             {
+//                 water += (rightmax - height[i]);
+//             }
+//             else
+//                 rightmax = height[i];
+//         }
+
+//         return water;
+//     }
+// };
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<    DAY-43     >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// print 2D Array in row-wise and in column-wise(create a function)
+// #include <iostream>
+// using namespace std;
+// void printCol(int arr[][3], int row, int col)
+// {
+//     for(int j=0;j<col;j++)
+//     {
+//         for(int i=0;i<row;i++)
+//         {
+//             cout << arr[i][j] << " ";
+//         }
+//     }
+// }
+// int main()
+// {
+// // create and initialize the array
+// int arr[4][3] = {1,2,3,4,5,6,7,8,89,0,12,3};
+// // print all the values in array in row-wise
+// for(int i=0;i<4;i++)
+// {
+// for(int j=0;j<3;j++)
+// cout << arr[i][j] << " ";
+// }
+// cout << endl;
+// printCol(arr,4,3);
+// }
+
+// sum of two matrix
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr1[2][3] = {23, 45, 23, 4, 54, 2};
+//     int arr2[2][3] = {2, 54, 2, 67, 45, 76};
+//     int ans[2][3];
+//     for (int i = 0; i < 2; i++)
+//     {
+//         for (int j = 0; j < 3; j++)
+//         {
+//             ans[i][j] = arr1[i][j] + arr2[i][j];
+//         }
+//     }
+//     for (int i = 0; i < 2; i++)
+//     {
+//         for (int j = 0; j < 3; j++)
+//         {
+//             cout << ans[i][j] << " ";
+//         }
+//         cout << "\n";
 //     }
 //     return 0;
 // }
 
+// print row index with maximum sum
+// #include <iostream>
+// #include <climits>
+// using namespace std;
 // int main()
 // {
-//     int n;
-//     cout << "Enter the size of Array: ";
-//     cin >> n;
-//     vector<int> arr(n);
-//     cout << "Enter the elements: ";
-//     for (int i = 0; i < n; i++)
-//         cin >> arr[i];
-//     cout << divide(arr);
+//     int arr[4][3]={12,324,45,23,45,23,21,76,34,3,4,5};
+//     int max_sum=INT_MIN, index = -1;
+//     for(int i=0;i<4;i++)
+//     {
+//         int sum =0;
+//         for(int j=0;j<3;j++)
+//             sum+=arr[i][j];
+//             if(sum>max_sum)
+//             {
+//                 max_sum=sum;
+//                 index = i;
+//             }
+//     }
+//     cout << "Max sum is: " << max_sum << " of index number: " << index;
 // }
 
-// 1. Kadane's Algorithm
-// https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
-// class Solution{
-//     public:
-//     // arr: input array
-//     // n: size of array
-//     //Function to find the sum of contiguous subarray with maximum sum.
-//     long long maxSubarraySum(int arr[], int n){
-//         // Your code here
-//         long long maxi = INT_MIN, prefix = 0;
-//         for(int i=0;i<n;i++)
+// print sum of diagonal elements
+// #include <iostream>
+// using namespace std;
+// int secDiagonal(int arr[][3],int  row, int col)
+// {
+//     int second = 0;
+//    int i=0, j=col-1;
+//    while(j>=0)
+//    {
+//     second+=arr[i][j];
+//    i++, j--;
+//    } return second;
+// }
+// int main()
+// {
+//     int arr[3][3] = {23,34,2,56,7,34,21,4,65};
+//     int first = 0;
+//     for(int i=0;i<3;i++)
+//     first+= arr[i][i];
+//     cout << first << endl;
+
+//     //second diagonal
+//     cout << secDiagonal(arr,3,3);
+// }
+
+// reverse each row of the matrix
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[3][4]={3,3,45,34,5,6,7,234,4,5,65,56};
+//     for(int i=0;i<3;i++)
+//     {
+//         for(int j=0;j<4;j++)
 //         {
-//             prefix+=arr[i];
-//             maxi = max(maxi,prefix);
-//             if(prefix < 0)
-//             prefix = 0;
+//             cout<<arr[i][j]<<" ";
 //         }
-//         return maxi;
+//         cout<<endl;
 //     }
-// };
+//     cout << "After reversing\n";
+//     for(int i=0;i<3;i++)
+//     {
+//         int start =0, end = 4-1;
+//         while(start<end)
+//         {
+//             swap(arr[i][start],arr[i][end]);
+//             start++, end--;
+//         }
+//     }
+//     for(int i=0;i<3;i++)
+//     {
+//         for(int j=0;j<4;j++)
+//         {
+//             cout<<arr[i][j]<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     }
--- README.md
+++ README.md
@@ -1,16 +1,33 @@
-Today is Day-39 and Today I learned about Prefix and Suffix...
+Today is Day-40 & Day-43 and Today I learned about Trapping Rain Water || Triplet Sum in Array || Introduction to 2D Arrays...
 Today's HW is:
 
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DAY-39>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DAY-40>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-1:Kadane's Algorithm
-//https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
+1:Trapping Rain Water
+//https://leetcode.com/problems/trapping-rain-water/description/
+2:3 SUM
+//https://practice.geeksforgeeks.org/problems/triplet-sum-in-array-1587115621/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
+3:4 SUM
+//https://practice.geeksforgeeks.org/problems/four-elements2452/1
+4: Remove Element from Array
+//https://www.interviewbit.com/problems/remove-element-from-array/
+5:Array 3 Pointers
+//https://www.interviewbit.com/problems/array-3-pointers/
+6: Container With Most Water
+//https://www.interviewbit.com/problems/container-with-most-water/
 
-2: Maximum Difference between 2 element / (We only have article for 2nd problem, so solve it on your local compiler)
-//https://www.geeksforgeeks.org/maximum-difference-between-two-elements
 
-3:Maximum prefix sum for a given range:
-//https://practice.geeksforgeeks.org/problems/maximum-prefix-sum-for-a-given-range0227/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DAY-43>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-4:Equal Sums:
-//https://practice.geeksforgeeks.org/problems/equal-sums4801/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
+1: Print sum of each column in 2D array.
+2: Given 2 matrices A and B, Print A-B.
+3: Given a matrix of size n*n, Print sum of diagonal element.
+ Ex:   1 2 3
+       4 5 6
+       7 8 9
+
+Its answer: 1+5+9 , 3+5+7, So the total sum will be 1+5+9+3+5+7 = 30. Here we can see that 5 is included 2 times, so we should include it only 1 time so the final answer will be, 30-5 = 25.  So the final answer will be 25.
+
+4: What is the column major order? 
+5: Largest Element: Find and print the largest element in the 2D array.
+6: Smallest Element: Find and print the smallest element in the 2D arrays. 
