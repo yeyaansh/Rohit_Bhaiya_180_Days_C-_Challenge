363b7410af9992af9e0f7d43f8854055e3a29507
Yeyaansh Patel <yeyaansh@gmail.com>
2023-11-27 05:42:19 +0530

    DAY-46

--- 180_days_challenge.cpp
+++ 180_days_challenge.cpp
@@ -1,138 +1,60 @@
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<   DAY-44    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<   DAY-46    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-// #include <iostream>
-// #include <vector>
-// #include <algorithm>
-// using namespace std;
-// int main()
-// {
-//     // declare , initialize by 3 and print them..
-//     vector<vector<int>>arr(4,vector<int>(5,3));
-//     for(int i=0;i<4;i++)
-//     for(int j=0;j<5;j++)
-//     cout << arr[i][j] << " ";
-
-//     // find out the rows and columns
-//     cout << "Row = " << arr.size();
-//     cout << endl;
-//     cout << "Col = " << arr[0].size();
-// }
-
-// // print wave form
-// #include <iostream>
-// using namespace std;
-// void wave(int arr[][5], int row , int col)
-// {
-//     for(int i=0;i<col;i++)
-//     {
-//         if(i%2==0)
-//         {
-//             // even
-//             for(int j=0;j<row;j++)
-//             {
-//                 cout << arr[j][i] << " ";
-//             }
-//         }
-//         else
-//         {
-//             // odd
-//             for(int j=row-1;j>=0;j--)
-//             {
-//                 cout << arr[j][i] << " ";
-//             }
-//         }
-//     }
-// }
-
-
-
-//<<<<<<<<<<<<<<<<<<<<<<<<< DAY-45 >>>>>>>>>>>>>>>>>>>>>>>>>>>  
-// // 1. Rotate Image
-// // https://leetcode.com/problems/rotate-image/description/
+// // 1. Binary Search in 2D Array
+// // https://leetcode.com/problems/search-a-2d-matrix/
 // class Solution {
 // public:
-//     void rotate(vector<vector<int>>& matrix) {
-//         int n = matrix.size();
-//         // transpose it
-//         for(int i=0;i<n-1;i++)
-//         {
-//             for(int j=i+1;j<n;j++)
-//             {
-//                 swap(matrix[i][j],matrix[j][i]);
-//             }
-//         }
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
 
-//         // reverse each row
-//         for(int i=0;i<n;i++)
+//         int m = matrix.size(), n = matrix[0].size();
+//         int start = 0, end = m*n-1;
+//         while(start <= end)
 //         {
-//             int start = 0, end = n-1;
-//             while(start<end)
-//             {
-//                 swap(matrix[i][start],matrix[i][end]);
-//                 start++, end--;
-//             }
-//         }
-//     }
-// };
+//             int mid = (start + end)/2;
+//           int  row = mid/n;
+//            int  col = mid%n;
 
+//             if(matrix[row][col] == target)
+//             return 1;
 
-// // 2. Rotate Matrix by 180
-// // https://practice.geeksforgeeks.org/problems/c-matrix-rotation-by-180-degree0745/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
-// class Solution {
-// public:
-//     void rotate(vector<vector<int> >& matrix) {
-//         // Code here
-//         int n = matrix.size();
-//         // column reverse
-//         for(int j=0;j<n;j++)
-//         {
-//             int start = 0, end = n-1;
+//             else if(matrix[row][col] < target)
+//             start = mid + 1;
             
-//             while(start<end)
-//             {
-//                 swap(matrix[start][j],matrix[end][j]);
-//                 start++, end--;
-//             }
+//             else
+//             end = mid -1;
 //         }
-//         // row reverse
-//         for(int i=0;i<n;i++)
-//         {
-//             int start = 0, end = n-1;
-//             while(start<end)
-//             {
-//                 swap(matrix[i][start],matrix[i][end]);
-//                 start++, end--;
-//             }
-//         }
+
+//         return 0;
+        
 //     }
 // };
 
-// 3. Rotate 90 degree anti-clockwise
-// https://practice.geeksforgeeks.org/problems/rotate-by-90-degree-1587115621/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
-// class Solution
-// {   
+
+// // 2. Search in a sorted row-col wise Matrix
+// // https://practice.geeksforgeeks.org/problems/search-in-a-matrix-1587115621/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
+// // class Solution
+// {
 //     public:
-//     //Function to rotate matrix anticlockwise by 90 degrees.
-//     void rotateby90(vector<vector<int> >& matrix, int n) 
-//     { 
-//         // code here 
-//         // transpose of a matrix
-//         for(int i=0;i<n-1;i++)
-//         {
-//             for(int j=i+1;j<n;j++)
-//             {
-//                 swap(matrix[i][j],matrix[j][i]);
-//             }
-//         }
-//         // reverse column elements
-//         for(int j=0;j<n;j++)
-//         {
-//             int start = 0, end = n-1;
-//             while(start<end)
-//             {
-//                 swap(matrix[start][j],matrix[end][j]);
-//                 start++, end--;
-//             }
-//         }
-//     } 
+//     //Function to search a given number in row-column sorted matrix.
+//     bool search(vector<vector<int> > matrix, int row, int col, int target) 
+//     {
+//         // code here
+//        int i = 0, j= col-1;
+//        while(i<row && j>=0)
+//        {
+//            if(matrix[i][j] == target)
+//            return 1;
+//            else if(matrix[i][j] > target)
+//            {
+//                // move left
+//                j = j-1;
+//            }
+//            else if(matrix[i][j] < target )
+//            {
+//                // move down
+//                i = i+1;
+//            }
+//        }
+//        return 0;
+//     }
 // };
--- README.md
+++ README.md
@@ -1,45 +1,23 @@
-Today is Day-44 & Day-45 and Today I learned about Column Major Order, Vector in 2D, questions on 2D arrays and Rotating the array..
+Today is Day-46 and Today I learned about Binary Search in 2D Array...
 Today's HW is:
 
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DAY-44>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DAY-46>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-Day 44/180 2D arrays Interview Problems
+Day 46/180 Binary Search in 2D Arrays
 
-1:Transpose of Matrix: ( Use lower triangle for Transpose)
-https://practice.geeksforgeeks.org/problems/transpose-of-matrix-1587115621/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
+1:Binary Search:
+https://leetcode.com/problems/search-a-2d-matrix/description/
 
-2: Given a Matrix of size n\*m (n=rows and m= cols). Reverse each column of the matrix.
+2:Search in a sorted row-col wise Matrix:
+https://practice.geeksforgeeks.org/problems/search-in-a-matrix-1587115621/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
 
-Example:
-1 2 3 ||7 8 9
-4 5 6 ||→ 4 5 6
-7 8 9 ||1 2 3
+3: Count zeros in a sorted matrix
+https://practice.geeksforgeeks.org/problems/count-zeros-in-a-sorted-matrix/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
 
-3: Spiral Matrix:
-https://leetcode.com/problems/spiral-matrix/
+4: Row with max 1s
+https://practice.geeksforgeeks.org/problems/row-with-max-1s0023/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
 
-4: Spiral Matrix II:
-https://leetcode.com/problems/spiral-matrix-ii/
+5: Binary Search: ( Solve it in log(n)+log(m) time, where n is number of row and m is number of columns)
+https://leetcode.com/problems/search-a-2d-matrix/description/
 
-5: Print Diagonally
-https://practice.geeksforgeeks.org/problems/print-diagonally4331/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
-
-6: Print matrix in diagonal pattern (Hard level)
-https://practice.geeksforgeeks.org/problems/print-matrix-in-diagonal-pattern/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
-
-7: Print Matrix in snake Patter
-https://practice.geeksforgeeks.org/problems/print-matrix-in-snake-pattern-1587115621/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
-
-Day 45/180 Rotate Matrix in C++
-
-1: Rotate Image:
-https://leetcode.com/problems/rotate-image/
-
-2: Matrix Rotation by 180 degree:
-https://practice.geeksforgeeks.org/problems/c-matrix-rotation-by-180-degree0745/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
-
-3: Rotate by 90 degree anticlockwise:
-https://practice.geeksforgeeks.org/problems/rotate-by-90-degree-1587115621/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
-
-4:Left Rotate Matrix K times (This is different Problem which was taught in class)
-https://practice.geeksforgeeks.org/problems/left-rotate-matrix-k-times2351/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
+6: Binary Search in a 2D array which is sorted in decreasing order. N is the number of rows and M is the number of columns.
