9181e38ba4ab58575d0ecf54ae8cd6ec02e828e0
Yeyaansh Patel <yeyaansh@gmail.com>
2024-01-09 05:01:43 +0530

    DAY-64

--- 180_days_challenge.cpp
+++ 180_days_challenge.cpp
@@ -1,25 +1,93 @@
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<   DAY-61    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<   DAY-64    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-#include <iostream>
-using namespace std;
-int main()
-{
-    // int a;
-    // // Print the address of a 
-    // cout << &a;
-    // cout << endl;
-    // int *ptr = &a;
-    // cout << ptr << endl;
-    // float m = 2.6;
-    // float *ptr1 = &m;
-    // cout << ptr1 << endl;
-    // cout << sizeof(ptr);
-    // int a = 10;
-    // int *ptr = &a;
-    // cout << ptr << endl;
-    // cout << *ptr;
-    // int b = 30;
-    // ptr = &b;
-    // cout << ptr << endl;
-    // cout << *ptr << endl;
-}
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[5] = {1,2,3,4,5};
+
+//     // Print the address of first element or 0th index element
+
+//     cout << arr << endl;
+//     cout << (arr+0) << endl;
+//     cout << &arr[0] << endl;
+//     int *ptr = &arr[0];
+//     cout << ptr << endl;
+
+//     // Print the address of second element or 1st index element
+
+//     cout << (arr+1) << endl;
+//     cout << &arr[1] << endl;
+//     cout << &*(arr+1) << endl;
+
+//     // Print the value of 0th index element
+
+//     cout << arr[0] << endl;
+//     cout << *(arr+0) << endl;
+    
+//     // Print all the addresses
+
+//     for(int i=0;i<5;i++)
+//     cout << arr+i << endl;
+
+//     // Print all the values
+
+//     for(int i=0;i<5;i++)
+//     cout << *(arr+i) << endl;
+
+// }
+
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[] = {1,2,33,4,99};
+//     int *ptr = arr;
+//     for (int i=0;i<5;i++)
+//     {
+//         // this should print the values of 1,2,33,4,99
+//         cout << ptr[i] << ", ";
+//     }
+//     cout << endl;
+//     for(int i=0;i<5;i++)
+//     {
+//         // this should print the values of 1,2,33,4,99
+//         cout << *(ptr+i) << ", ";
+//     }
+//     cout << endl;
+//     for (int i=0;i<5;i++)
+//     {
+//         // this should also print the values of 1,2,33,4,99
+//         cout << *ptr << ", ";
+//         ptr++;
+//     }
+//     cout << endl;
+//     // to print only their address
+//     for(int i=0;i<5;i++)
+//     {
+//         cout << (ptr+i) << ", ";
+//     }
+
+//        cout << endl;
+//     // to print only their addresses using arithmetic operator
+//     for(int i=0;i<5;i++)
+//     {
+//         cout << ptr << ", ";
+//         ptr++;
+//     }
+// }
+
+
+// THIS IS HOW I DID THE HOMEWORK QUESTION GIVEN IN VIDEO   
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     char arr[5] = {"1234"};
+//     char *ptr = arr;
+//     cout << *ptr << " ";
+//     cout << endl;
+//     char ch = 'a';
+//     cout << *(&ch) << " ";
+// }
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-Today is Day-61 and Today I learned about Pointers...
+Today is Day-64 and Today I learned about Pointers Relationship with array || Arithmetic Pointers...
 Today's HW is:
-What is a Symbol Table and learn about it
+1: Print the address of the first index of the char array in c++.
+2: Print the address of the char variable in c++.
+3: Print the address of String in c++.
+4: Given an array in c++
+
+int arr[5] = {1,2,3,4,5};
+
+What is the difference between arr and &arr. Try to explore it on your own.
+
+5: int arr[5] = {1,2,3,4,5};
+Create a pointer which will store the address of the last element. Then print all the elements from the last index to 0th index with the help of a pointer.
